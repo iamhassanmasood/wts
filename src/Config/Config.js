a51b37904a85ed3474e83497d08639d5770c6893
Hassan Masood <hassanmasood@renesistech.com>
2020-04-08 19:06:13 +0500

    Changed Configuration

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = `https://wts.cs-satms.com`;
+export const BASE_URL = `http://staging-wats.cs-satms.com`;
 export const PORT = 8443;
 export const SITES_API = `api/sites`;
 export const REGIONS_API = `api/regions`;
@@ -7,9 +7,9 @@ export const ALERTS_API = `api/alerts`;
 export const token = localStorage.getItem('accessToken');
 export const headers = { 'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': 'Bearer ' + token };
 export const ASSET_API = `api/assets`
-export const TRANSFER_ASSET = `assets_transfer?asset_id=&site_id=`
+export const TRANSFER_ASSET = `assets_transfer`
 export const ASSET_BY_SITE = `assets_states_report_by_site_id?site_id=`
 export const LOGIN = `o/token`
-export const CLIENT_SECRET = "bm1Tjsd3awm3k3ZilIsOB3Qz1QbKOrnFfzxUvLgGtjGDswIzK5T7djRTVYNcO3nXOtacq3RtKwM2i8dZBxtkw1Onw3jcj9GVDZUuJ0MPpZJ7CgrWvOKsRu50bJHBpfSn";
-export const CLIENT_ID = "3pXWuFFg18vj3SL46WDCpZ14tFC1Xl9ousG8Z4lu";
+export const CLIENT_SECRET = "tM8i79al4DXRX5l6ywJAYqnw17giqjCx9gVjNjmyJXunA0rpscuolQxkGfdXIzLjDcWO3HAN3GT4ZGOYXl0QmtHZBw8KLJQJVqVpoqQnk3wCg5dsh0DpV5UPYLaLg1vP";
+export const CLIENT_ID = "dLrXEw6WIj9shLkbXHfngd4LrRX1EzGVl57t91gu";
 export const ReportAPI = `alerts_reporting`
