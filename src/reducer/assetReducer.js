6d7093afca34f82cacfacd31efdca756415d4a58
Hassan Masood <hassanmasood@renesistech.com>
2020-04-21 10:09:52 +0500

    For Alerts Redux Setup

--- src/reducer/assetReducer.js
+++ src/reducer/assetReducer.js
@@ -1,10 +1,22 @@
-let assetReducer = (state = [], actions) => {
+import { ADD_ASSET, REMOVE_ASSET, UPDATE_ASSET, ASSET_DATA } from '../actions/actionType'
+let initialState = {
+    AssetData: [],
+    fetching: false
+};
+
+let assetReducer = (state = initialState, actions = null) => {
     switch (actions.type) {
-        case "ADD_ASSET":
+        case ASSET_DATA:
+            return {
+                ...state,
+                AssetData: actions.payload.data.data,
+                fetching: false
+            }
+        case ADD_ASSET:
             return state
-        case "REMOVE_ASSET":
+        case REMOVE_ASSET:
             return state
-        case "UPDATE_ASSET":
+        case UPDATE_ASSET:
             return state
         default:
             return state
