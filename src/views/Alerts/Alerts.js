37a42c06c3103d2e9a364a12ac061151de636a87
Hassan Masood <hassanmasood@renesistech.com>
2020-04-23 17:47:04 +0500

    Alermanagement : Global Function Res Error

--- src/views/Alerts/Alerts.js
+++ src/views/Alerts/Alerts.js
@@ -1,26 +1,13 @@
 import React, { useEffect, useState, Fragment } from 'react';
-import { BASE_URL, FORMAT, ALERTS_API, SITES_API } from '../../Config/Config'
+import { BASE_URL, FORMAT, ALERTS_API } from '../../Config/Config'
 import { Pagination } from 'antd';
-
-import axios from 'axios'
-import AlertsModule from './AlertsModule';
+import { timeConverter } from '../../GlobalFunctions/timeConverter'
+import axios from 'axios'; import AlertsModule from './AlertsModule';
 
 export default function Alerts() {
 
-  const timeConverter = (UNIX_timestamp) => {
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
-    return time;
-  }
-
   const [alerts, setAlerts] = useState([]);
+  const [hasError, setErrors] = useState(false);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [alertsPerPage, setAlertsPerPage] = useState(10);
@@ -31,7 +18,11 @@ export default function Alerts() {
       var headers = { 'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': 'Bearer ' + token }
       setLoading(true);
       const res = await axios.get(`${BASE_URL}/${ALERTS_API}/${FORMAT}`, { headers });
-      setAlerts(res.data.data);
+      if (res.status === 200) {
+        setAlerts(res.data.data);
+      } else {
+        setErrors(true)
+      }
       setLoading(false);
     };
 
@@ -42,25 +33,25 @@ export default function Alerts() {
   const indexOfFirstAlert = indexOfLastAlert - alertsPerPage;
   const currentAlerts = alerts.slice(indexOfFirstAlert, indexOfLastAlert);
   const paginate = pageNumber => setCurrentPage(pageNumber);
-
   return (
-    <Fragment>
+    < Fragment >
       <AlertsModule
         Data={currentAlerts}
-        timeConverter={e => timeConverter(e)}
+        timeConverter={timeConverter}
         loading={loading}
         page={currentPage}
         alertsPerPage={alertsPerPage} />
-      {!loading ?
-        <Pagination
-          showQuickJumper
-          defaultPageSize={10}
-          defaultCurrent={2}
-          pageSize={alertsPerPage}
-          total={alerts.length}
-          onChange={paginate} /> : ''}
-
-    </Fragment>
+      {
+        !loading ?
+          <Pagination
+            showQuickJumper
+            defaultPageSize={10}
+            defaultCurrent={2}
+            pageSize={alertsPerPage}
+            total={alerts.length}
+            onChange={paginate} /> : ''
+      }
+    </Fragment >
   )
 
 }
