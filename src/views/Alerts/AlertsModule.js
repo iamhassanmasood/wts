696269613dbef35326a2824a3e34f99724d9f14c
Hassan Masood <hassanmasood@renesistech.com>
2020-05-13 23:35:22 +0500

    ALERTS:CSV download with file name according to

--- src/views/Alerts/AlertsModule.js
+++ src/views/Alerts/AlertsModule.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
+import { Card, CardBody, CardHeader, Col, Row, Table, Button } from 'reactstrap';
+import { CSVLink } from "react-csv";
 
-
-export default function AlertsModule({ Data, timeConverter, loading, alertsPerPage, page }) {
+export default function AlertsModule({ Data, timeConverter, loading, alertsPerPage, page, Headers, csvData }) {
   if (Data) {
     var rows = Data.reverse().map((alt, i) => {
       var AlertTimeNow;
@@ -33,6 +33,9 @@ export default function AlertsModule({ Data, timeConverter, loading, alertsPerPa
           <Card>
             <CardHeader>
               <i className="fa fa-bell"></i> Alerts
+              <CSVLink data={csvData} headers={Headers} filename={"Alerts.csv"} className='card-header-actions'>
+                <Button color="primary" size="sm" className="btn-pill">Export CSV</Button>
+              </CSVLink>
             </CardHeader>
             <CardBody>
               <Table hover bordered striped responsive size="sm" className="table table-striped table-dark">
@@ -43,7 +46,7 @@ export default function AlertsModule({ Data, timeConverter, loading, alertsPerPa
                     <th>Asset Id </th>
                     <th>Registered Site Id</th>
                     <th>Site Id</th>
-                    <td>Alert Type</td>
+                    <th>Alert Type</th>
                     <th>Created At</th>
                   </tr>
                 </thead>
@@ -58,3 +61,13 @@ export default function AlertsModule({ Data, timeConverter, loading, alertsPerPa
     </div>
   )
 }
+
+/***
+Hello Yasir!, I have posted a mutual friend's comment abt your personality according to your
+idealogical thoughts pattren nothing more like to attack on your personal life.
+I have just posted it the unfortunate mistake which we have made is your permission we appologise for that.
+ if you think it's attack on your current life so please tell me i'll remove it.
+ If you think it's your own metter no one has right to discuss it then please think abt
+ forcefully implementing your thoughts on anyone at planet by your gribness.
+ Everyone has own ideas and follow any theory by own choice no one has right to disgrac him or devalue him.
+ */
